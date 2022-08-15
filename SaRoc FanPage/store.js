73637b571dce15272bf995a742801a38bf243e46
seryxme <coolbams24@yahoo.com>
2022-08-15 14:20:02 +0100

    My new web codes

---
+++ SaRoc FanPage/store.js
@@ -0,0 +1,8 @@
+var cartItemRemover = document.getElementsByClassName('btn-danger')
+console.log(cartItemRemover)
+for (var i = 0; i < cartItemRemover.length; i++) {
+    var button = cartItemRemover[i]
+    button.addEventListener('click', function() {
+        
+    })
+}
