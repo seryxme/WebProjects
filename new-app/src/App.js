28bcfbfc28c4cc39cf7595bbf6acfc15d89450eb
seryxme <coolbams24@yahoo.com>
2022-10-14 15:09:58 +0100

    new app codes

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React</h1>
+      <p>
+        <h2>React-shun time!</h2>
+      </p>
+      <label for="name">Name</label>
+      <input id="name" placeholder="Enter your name" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
     </div>
   );
-}
+};
 
 export default App;
