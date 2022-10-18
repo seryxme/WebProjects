6c060e8091d4826005a90d2b9d842b3eca4a92a0
seryxme <coolbams24@yahoo.com>
2022-10-18 09:34:19 +0100

    updates

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import Nav from "./components/Nav";
 import CardList from "./components/Card/CardList";
 import NewCard from "./components/NewCard";
-import Counter from "./components/Counter/Counter";
+// import Counter from "./components/Counter/Counter";
 
 const App = () => {
   return (
     <div>
       <Nav />
       <NewCard />
-      <Counter />
-      {/* <CardList /> */}
+      {/* <Counter /> */}
+      <CardList />
     </div>
   );
 };
